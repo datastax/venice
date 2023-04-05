1defcc1609f1fb52e83a95036f5ad76bf9adb86e
Enrico Olivelli <eolivelli@gmail.com>
2023-04-05 14:58:36 +0200

    [docker] Add Prometheus exporter jars to Docker images

--- docker/build-venice-docker-images.sh
+++ docker/build-venice-docker-images.sh
@@ -5,21 +5,21 @@ cd docker
 repository="${1:-venicedb}"
 oss_release="${2:-0.4.17}"
 
-set -x
+set -x -e
 echo "Building docker images for repository $repository, version $oss_release"
 
 head_hash=$(git rev-parse --short HEAD)
 version=$oss_release
 
-cp *py venice-client/ 
+cp *py *yaml venice-client/ 
 cp ../clients/venice-push-job/build/libs/venice-push-job-all.jar venice-client/
 cp ../clients/venice-thin-client/build/libs/venice-thin-client-all.jar venice-client/
 cp ../clients/venice-admin-tool/build/libs/venice-admin-tool-all.jar venice-client/
-cp *py venice-server/ 
+cp *py *yaml venice-server/ 
 cp ../services/venice-server/build/libs/venice-server-all.jar venice-server/
-cp *py venice-controller/ 
+cp *py *yaml venice-controller/ 
 cp ../services/venice-controller/build/libs/venice-controller-all.jar venice-controller/
-cp *py venice-router/ 
+cp *py *yaml venice-router/ 
 cp ../services/venice-router/build/libs/venice-router-all.jar venice-router/
 
 targets=(venice-controller venice-server venice-router venice-client)
@@ -34,7 +34,7 @@ rm -f venice-client/venice-admin-tool-all.jar
 rm -f venice-server/venice-server-all.jar
 rm -f venice-controller/venice-controller-all.jar
 rm -f venice-router/venice-router-all.jar
-rm */*.py
+rm */*.py */*yaml
 
 for target in ${targets[@]}; do
    docker push $repository/$target:$version
