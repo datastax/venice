e128e1f6a4bc0964004626dedf1b234a80bd48e2
Nicolò Boschi <boschi1997@gmail.com>
2023-05-19 09:55:12 +0200

    [router] some SSLConfig fields are not considered (#18)
    It's not possible to configure the ssl field `sslRequireClientCerts` via
    configuration.
    With the new authentication provider, mTLS auth is not required when TLS
    is enabled and therefore must be configurable in the router component.
    
    Changes: 
    * Copy the value from the properties
    * Reduce log for `SSLPeerUnverifiedException` when
    `needClientAuth=false`
    * Allow storeAcl (both for Router and Server) to perform acl without a
    client certificate

--- services/venice-server/src/main/java/com/linkedin/venice/listener/ServerStoreAclHandler.java
+++ services/venice-server/src/main/java/com/linkedin/venice/listener/ServerStoreAclHandler.java
@@ -4,16 +4,12 @@ import com.linkedin.venice.acl.DynamicAccessController;
 import com.linkedin.venice.acl.handler.StoreAclHandler;
 import com.linkedin.venice.authentication.AuthenticationService;
 import com.linkedin.venice.authorization.AuthorizerService;
-import com.linkedin.venice.exceptions.VeniceException;
 import com.linkedin.venice.meta.ReadOnlyStoreRepository;
 import com.linkedin.venice.meta.Version;
-import com.linkedin.venice.utils.SslUtils;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.ssl.SslHandler;
 import io.netty.util.Attribute;
 import io.netty.util.ReferenceCountUtil;
-import java.security.cert.X509Certificate;
 import java.util.Optional;
 import javax.net.ssl.SSLPeerUnverifiedException;
 
@@ -53,16 +49,6 @@ public class ServerStoreAclHandler extends StoreAclHandler {
       ctx.fireChannelRead(req);
     } else {
       super.channelRead0(ctx, req);
-    }
-  }
-
-  @Override
-  protected X509Certificate extractClientCert(ChannelHandlerContext ctx) throws SSLPeerUnverifiedException {
-    SslHandler sslHandler = ServerHandlerUtils.extractSslHandler(ctx);
-    if (sslHandler != null) {
-      return SslUtils.getX509Certificate(sslHandler.engine().getSession().getPeerCertificates()[0]);
-    } else {
-      throw new VeniceException("Failed to extract client cert from the incoming request");
     }
   }
 
